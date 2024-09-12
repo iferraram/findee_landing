9ed911c58c971c30f4b0aa11c9bc945d778e4f98
iferraram <isaferraram@gmail.com>
2024-09-11 21:02:21 -0600

    name fix

--- src/components/gradient/gradient.tsx
+++ src/components/gradient/gradient.tsx
@@ -4,7 +4,7 @@ import demo from '/assets/findee_landing.gif'
 function Gradient({ className }: { className?: string }) {
   return (
     <div className={cn('hidden w-1/2 md:block', className)} style={{ backgroundColor: '#ffbd59' }}>
-      <div className="relative -top-10 hidden md:mt-16 lg:block">
+      <div className="b relative -top-10 hidden md:mt-16 lg:block">
         <img
           src={demo}
           alt="Findee"

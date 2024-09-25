1d19c23dd7fac36f974e3eea8d97cbb858b797da
iferraram <isaferraram@gmail.com>
2024-09-24 21:17:21 -0600

    removed apple storet

--- src/components/header/CarouselItem.tsx
+++ src/components/header/CarouselItem.tsx
@@ -7,14 +7,14 @@ export default function CarouselItem({ scroll, icon, title, text }) {
         <div className="align-center flex h-full flex-col dark:text-white lg:flex-row lg:justify-center">
           <div className="flex flex-col px-3 lg:justify-center lg:px-10">
             <img src={icon} alt="Findee" className="m-auto w-4/6 md:w-2/6 lg:hidden" />
-            <button
+            {/* <button
               className="m-auto mb-5 w-2/6 md:hidden"
               onClick={() =>
                 window.open('https://apps.apple.com/mx/app/findee/id6502679719', '_blank')
               }
             >
               <img src={appleStore} alt="Findee" className="m-auto w-56 md:w-56 lg:hidden" />
-            </button>
+            </button> */}
           </div>
           <div className="flex w-screen flex-col justify-center bg-brand p-3 shadow-lg md:h-56 lg:h-full lg:w-1/2 lg:flex-row lg:flex-col lg:bg-white lg:shadow-none">
             <h2 className="mt-2 text-center text-lg font-bold lg:text-left lg:text-3xl">{title}</h2>
@@ -29,14 +29,14 @@ export default function CarouselItem({ scroll, icon, title, text }) {
           </div>
           <div className="mt-5 flex max-w-xs flex-col items-center justify-center">
             <img src={icon} alt="Findee" className="hidden h-auto lg:block lg:w-auto" />
-            <button
+            {/* <button
               className="hidden h-auto md:block md:w-48"
               onClick={() =>
                 window.open('https://apps.apple.com/mx/app/findee/id6502679719', '_blank')
               }
             >
               <img src={appleStore} alt="Findee" className="hidden h-auto md:block md:w-48" />
-            </button>
+            </button> */}
           </div>
         </div>
       </div>
